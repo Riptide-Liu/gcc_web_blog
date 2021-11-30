9801b38b7655fde6cea973d046eecfa5a482a665
lamgr <lamgradon@Hotmail.com>
2021-11-30 10:59:54 +0800

    hello

--- src/main/java/com/web/blog/BlogApplication.java
+++ src/main/java/com/web/blog/BlogApplication.java
@@ -9,6 +9,7 @@ public class BlogApplication {
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
         //123123123123
+        //bbb
     }
 
 }
